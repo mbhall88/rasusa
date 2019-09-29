06216f76f5eb9d3effa37c606b86709d9fb33cc5
Michael Hall <michael@mbh.sh>
2019-09-29 17:41:20 +0100

    added coverage cli arg and parser

--- src/cli.rs
+++ src/cli.rs
@@ -53,8 +53,34 @@ fn parse_genome_size(s: String) -> Result<u64, String> {
     }
 }
 
-fn is_valid_genome_size(arg: String) -> Result<(), String> {
+fn is_genome_size_valid(arg: String) -> Result<(), String> {
     match parse_genome_size(arg) {
+        Ok(_) => Ok(()),
+        Err(msg) => Err(msg),
+    }
+}
+
+fn parse_coverage(arg: String) -> Result<u32, String> {
+    let re = Regex::new(r"^(?P<covg>[0-9]*\.?[0-9]+)(?i)x?$").unwrap();
+    let captures = match re.captures(arg.as_str()) {
+        Some(cap) => cap,
+        None => {
+            return Err(
+                "Coverage should be an int or float and can end in an x character.".to_string(),
+            );
+        }
+    };
+    match captures.name("covg") {
+        Some(x) => Ok(x.as_str().parse::<f64>().unwrap() as u32),
+        None => {
+            Err("Coverage should be an int or float and can end in an x character.".to_string())
+        }
+    }
+}
+
+fn is_coverage_valid(arg: String) -> Result<(), String> {
+    match parse_coverage(arg) {
+        Ok(0) => Err("Coverage of 0 given.".to_string()),
         Ok(_) => Ok(()),
         Err(msg) => Err(msg),
     }
@@ -76,7 +102,8 @@ where
                 .value_name("INPUT")
                 .help("The fastq file to sub-sample.")
                 .required(true)
-                .takes_value(true),
+                .takes_value(true)
+                .display_order(1),
         )
         .arg(
             Arg::with_name("genome_size")
@@ -84,6 +111,7 @@ where
                 .long("genome-size")
                 .value_name("GENOME SIZE")
                 .help(
+                    // todo: fix formatting of text in terminal
                     "Size of the genome to calculate coverage with respect to. \
                      This can be either an integer or an integer/float with a metric suffix. \
                      An Example of this is 4.3kb will indicate a genome size of 4300. \
@@ -91,7 +119,17 @@ where
                 )
                 .required(true)
                 .takes_value(true)
-                .validator(is_valid_genome_size),
+                .validator(is_genome_size_valid),
+        )
+        .arg(
+            Arg::with_name("coverage")
+                .short("c")
+                .long("coverage")
+                .value_name("COVG")
+                .help("The desired coverage to sub-sample the reads to.")
+                .required(true)
+                .takes_value(true)
+                .validator(is_coverage_valid),
         )
         .arg(
             Arg::with_name("v")
@@ -207,48 +245,165 @@ mod tests {
     fn empty_string_is_invalid_genome_size() {
         let arg = String::from("");
 
-        assert!(is_valid_genome_size(arg).is_err())
+        assert!(is_genome_size_valid(arg).is_err())
     }
 
     #[test]
     fn int_only_is_valid() {
         let arg = String::from("789");
 
-        assert!(is_valid_genome_size(arg).is_ok())
+        assert!(is_genome_size_valid(arg).is_ok())
     }
 
     #[test]
     fn float_only_is_valid() {
         let arg = String::from("79.6");
 
-        assert!(is_valid_genome_size(arg).is_ok())
+        assert!(is_genome_size_valid(arg).is_ok())
     }
 
     #[test]
     fn int_with_valid_suffix_is_valid() {
         let arg = String::from("79g");
 
-        assert!(is_valid_genome_size(arg).is_ok())
+        assert!(is_genome_size_valid(arg).is_ok())
     }
 
     #[test]
     fn int_with_invalid_suffix_is_invalid() {
         let arg = String::from("79p");
 
-        assert!(is_valid_genome_size(arg).is_err())
+        assert!(is_genome_size_valid(arg).is_err())
     }
 
     #[test]
     fn float_with_valid_suffix_is_valid() {
         let arg = String::from("79.765t");
 
-        assert!(is_valid_genome_size(arg).is_ok())
+        assert!(is_genome_size_valid(arg).is_ok())
     }
 
     #[test]
     fn float_with_invalid_suffix_is_invalid() {
         let arg = String::from("79.765eb");
 
-        assert!(is_valid_genome_size(arg).is_err())
+        assert!(is_genome_size_valid(arg).is_err())
+    }
+
+    #[test]
+    fn int_coverage_returns_int() {
+        let arg = String::from("56");
+
+        let actual = parse_coverage(arg).unwrap();
+        let expected = 56 as u32;
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn float_coverage_returns_int() {
+        let arg = String::from("56.6");
+
+        let actual = parse_coverage(arg).unwrap();
+        let expected = 56 as u32;
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn empty_coverage_returns_err() {
+        let arg = String::from("");
+
+        let actual = parse_coverage(arg);
+
+        assert!(actual.is_err())
+    }
+
+    #[test]
+    fn non_number_coverage_returns_err() {
+        let arg = String::from("foo");
+
+        let actual = parse_coverage(arg);
+
+        assert!(actual.is_err())
+    }
+
+    #[test]
+    fn zero_coverage_returns_zero() {
+        let arg = String::from("0");
+
+        let actual = parse_coverage(arg).unwrap();
+        let expected = 0 as u32;
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn int_ending_in_x_coverage_returns_int() {
+        let arg = String::from("1X");
+
+        let actual = parse_coverage(arg).unwrap();
+        let expected = 1 as u32;
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn float_ending_in_x_coverage_returns_int() {
+        let arg = String::from("1.9x");
+
+        let actual = parse_coverage(arg).unwrap();
+        let expected = 1 as u32;
+
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn empty_string_is_invalid_coverage_string() {
+        let covg = String::from("");
+
+        assert!(is_coverage_valid(covg).is_err())
+    }
+
+    #[test]
+    fn zero_is_invalid_coverage_string() {
+        let covg = String::from("0");
+
+        assert!(is_coverage_valid(covg).is_err())
+    }
+
+    #[test]
+    fn int_is_valid_coverage_string() {
+        let covg = String::from("90");
+
+        assert!(is_coverage_valid(covg).is_ok())
+    }
+
+    #[test]
+    fn float_is_valid_coverage_string() {
+        let covg = String::from("90.9");
+
+        assert!(is_coverage_valid(covg).is_ok())
+    }
+
+    #[test]
+    fn word_is_invalid_coverage_string() {
+        let covg = String::from("foo");
+
+        assert!(is_coverage_valid(covg).is_err())
+    }
+
+    #[test]
+    fn int_ending_in_x_is_valid_coverage_string() {
+        let covg = String::from("50x");
+
+        assert!(is_coverage_valid(covg).is_ok())
+    }
+
+    #[test]
+    fn float_ending_in_x_is_valid_coverage_string() {
+        let covg = String::from("50.8X");
+
+        assert!(is_coverage_valid(covg).is_ok())
     }
 }
