00de57db20b51d0adb0428f01d9ee7b85d921c52
Michael Hall <michael@mbh.sh>
2019-10-03 17:16:31 +0100

    make Coverage and GenomeSize structs

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,8 @@ extern crate clap;
 use clap::{App, Arg, ArgMatches};
 use regex::Regex;
 use std::ffi::OsString;
+use std::ops::Mul;
+use std::str::FromStr;
 
 #[derive(PartialEq, Debug)]
 enum MetricSuffix {
@@ -13,78 +15,91 @@ enum MetricSuffix {
     Tera,
 }
 
-fn multiply_size_by_metric_suffix(
-    size: f64,
-    metric_suffix: Result<MetricSuffix, String>,
-) -> Result<f64, String> {
-    match metric_suffix {
-        Ok(MetricSuffix::Base) => Ok(size),
-        Ok(MetricSuffix::Kilo) => Ok(size * 1_000.0),
-        Ok(MetricSuffix::Mega) => Ok(size * 1_000_000.0),
-        Ok(MetricSuffix::Giga) => Ok(size * 1_000_000_000.0),
-        Ok(MetricSuffix::Tera) => Ok(size * 1_000_000_000_000.0),
-        Err(msg) => Err(msg),
-    }
-}
+impl Mul<MetricSuffix> for f64 {
+    type Output = Self;
 
-fn parse_genome_size(s: String) -> Result<u64, String> {
-    let text = s.to_lowercase();
-    let re = Regex::new(r"(?P<size>[0-9]*\.?[0-9]+)(?P<sfx>\w*)$").unwrap();
-    let captures = match re.captures(text.as_str()) {
-        Some(cap) => cap,
-        None => return Err("No matches found in genome size string".to_string()),
-    };
-    let size = captures
-        .name("size")
-        .unwrap()
-        .as_str()
-        .parse::<f64>()
-        .unwrap();
-    let metric_suffix = match captures.name("sfx") {
-        Some(x) if "b".contains(x.as_str()) => Ok(MetricSuffix::Base),
-        Some(x) if "kb".contains(x.as_str()) => Ok(MetricSuffix::Kilo),
-        Some(x) if "mb".contains(x.as_str()) => Ok(MetricSuffix::Mega),
-        Some(x) if "gb".contains(x.as_str()) => Ok(MetricSuffix::Giga),
-        Some(x) if "tb".contains(x.as_str()) => Ok(MetricSuffix::Tera),
-        None => Ok(MetricSuffix::Base),
-        _ => Err("Invalid metric suffix.".to_string()),
-    };
-    match multiply_size_by_metric_suffix(size, metric_suffix) {
-        Ok(x) => Ok(x as u64),
-        Err(msg) => Err(msg),
-    }
-}
-
-fn is_genome_size_valid(arg: String) -> Result<(), String> {
-    match parse_genome_size(arg) {
-        Ok(_) => Ok(()),
-        Err(msg) => Err(msg),
-    }
-}
-
-fn parse_coverage(arg: String) -> Result<u32, String> {
-    let re = Regex::new(r"^(?P<covg>[0-9]*\.?[0-9]+)(?i)x?$").unwrap();
-    let captures = match re.captures(arg.as_str()) {
-        Some(cap) => cap,
-        None => {
-            return Err(
-                "Coverage should be an int or float and can end in an x character.".to_string(),
-            );
+    fn mul(self, rhs: MetricSuffix) -> Self::Output {
+        match rhs {
+            MetricSuffix::Base => self,
+            MetricSuffix::Kilo => self * 1_000.0,
+            MetricSuffix::Mega => self * 1_000_000.0,
+            MetricSuffix::Giga => self * 1_000_000_000.0,
+            MetricSuffix::Tera => self * 1_000_000_000_000.0,
         }
-    };
-    Ok(captures
-        .name("covg")
-        .unwrap()
-        .as_str()
-        .parse::<f64>()
-        .unwrap() as u32)
+    }
 }
 
-fn is_coverage_valid(arg: String) -> Result<(), String> {
-    match parse_coverage(arg) {
-        Ok(0) => Err("Coverage of 0 given.".to_string()),
-        Ok(_) => Ok(()),
-        Err(msg) => Err(msg),
+#[derive(Debug)]
+struct GenomeSize(u64);
+
+impl PartialEq<u64> for GenomeSize {
+    fn eq(&self, other: &u64) -> bool {
+        self.0 == *other
+    }
+}
+
+impl FromStr for GenomeSize {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let text = s.to_lowercase();
+        let re = Regex::new(r"(?P<size>[0-9]*\.?[0-9]+)(?P<sfx>\w*)$").unwrap();
+        let captures = match re.captures(text.as_str()) {
+            Some(cap) => cap,
+            None => return Err(
+                "Invalid genome size format. Accepted formats include: 4000, 4mb, 7Gb, 6.7Kb etc."
+                    .to_string(),
+            ),
+        };
+        let size = captures
+            .name("size")
+            .unwrap()
+            .as_str()
+            .parse::<f64>()
+            .unwrap();
+        let metric_suffix = match captures.name("sfx") {
+            Some(x) if "b".contains(x.as_str()) => MetricSuffix::Base,
+            Some(x) if "kb".contains(x.as_str()) => MetricSuffix::Kilo,
+            Some(x) if "mb".contains(x.as_str()) => MetricSuffix::Mega,
+            Some(x) if "gb".contains(x.as_str()) => MetricSuffix::Giga,
+            Some(x) if "tb".contains(x.as_str()) => MetricSuffix::Tera,
+            None => MetricSuffix::Base,
+            _ => return Err("Invalid metric suffix.".to_string()),
+        };
+        Ok(GenomeSize((size * metric_suffix) as u64))
+    }
+}
+
+#[derive(Debug)]
+struct Coverage(u32);
+
+impl PartialEq<u32> for Coverage {
+    fn eq(&self, other: &u32) -> bool {
+        self.0 == *other
+    }
+}
+
+impl FromStr for Coverage {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"^(?P<covg>[0-9]*\.?[0-9]+)(?i)x?$").unwrap();
+        let captures = match re.captures(s) {
+            Some(cap) => cap,
+            None => {
+                return Err(
+                    "Coverage should be an int or float and can end in an x character.".to_string(),
+                );
+            }
+        };
+        Ok(Coverage(
+            captures
+                .name("covg")
+                .unwrap()
+                .as_str()
+                .parse::<f64>()
+                .unwrap() as u32,
+        ))
     }
 }
 
@@ -120,8 +135,7 @@ where
                      Other examples include 7Tb, 9.1gb, 90MB, 6009 etc.",
                 )
                 .required(true)
-                .takes_value(true)
-                .validator(is_genome_size_valid),
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("coverage")
@@ -130,8 +144,7 @@ where
                 .value_name("COVG")
                 .help("The desired coverage to sub-sample the reads to.")
                 .required(true)
-                .takes_value(true)
-                .validator(is_coverage_valid),
+                .takes_value(true),
         )
         .arg(
             Arg::with_name("v")
@@ -147,30 +160,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn integer_only_returns_integer() {
-        let arg = String::from("6");
+    fn float_multiply_with_base_unit() {
+        let actual = 4.5 * MetricSuffix::Base;
+        let expected = 4.5;
 
-        let actual = parse_genome_size(arg).unwrap();
-        let expected = 6;
+        assert_eq!(actual, expected)
+    }
+
+    #[test]
+    fn integer_only_returns_integer() {
+        let actual = GenomeSize::from_str("6").unwrap();
+        let expected = 6 as u64;
 
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn float_only_returns_integer() {
-        let arg = String::from("6.5");
-
-        let actual = parse_genome_size(arg).unwrap();
-        let expected = 6;
+        let actual = GenomeSize::from_str("6.5").unwrap();
+        let expected = 6 as u64;
 
         assert_eq!(actual, expected);
     }
 
     #[test]
     fn int_and_suffix_returns_multiplied_int() {
-        let arg = String::from("5mb");
-
-        let actual = parse_genome_size(arg).unwrap();
+        let actual = GenomeSize::from_str("5mb").unwrap();
         let expected = 5_000_000 as u64;
 
         assert_eq!(actual, expected);
@@ -178,9 +193,7 @@ mod tests {
 
     #[test]
     fn float_and_suffix_returns_multiplied_float_as_int() {
-        let arg = String::from("5.4kB");
-
-        let actual = parse_genome_size(arg).unwrap();
+        let actual = GenomeSize::from_str("5.4kB").unwrap();
         let expected = 5_400 as u64;
 
         assert_eq!(actual, expected);
@@ -188,9 +201,7 @@ mod tests {
 
     #[test]
     fn float_without_leading_int_and_suffix_returns_multiplied_float_as_int() {
-        let arg = String::from(".77G");
-
-        let actual = parse_genome_size(arg).unwrap();
+        let actual = GenomeSize::from_str(".77G").unwrap();
         let expected = 770_000_000 as u64;
 
         assert_eq!(actual, expected);
@@ -198,9 +209,7 @@ mod tests {
 
     #[test]
     fn int_and_tera_suffix_returns_multiplied_int() {
-        let arg = String::from("7TB");
-
-        let actual = parse_genome_size(arg).unwrap();
+        let actual = GenomeSize::from_str("7TB").unwrap();
         let expected = 7_000_000_000_000 as u64;
 
         assert_eq!(actual, expected);
@@ -208,9 +217,7 @@ mod tests {
 
     #[test]
     fn int_and_base_suffix_returns_int_without_scaling() {
-        let arg = String::from("7B");
-
-        let actual = parse_genome_size(arg).unwrap();
+        let actual = GenomeSize::from_str("7B").unwrap();
         let expected = 7 as u64;
 
         assert_eq!(actual, expected);
@@ -218,85 +225,28 @@ mod tests {
 
     #[test]
     fn invalid_suffix_returns_err() {
-        let arg = String::from(".77uB");
-
-        let actual = parse_genome_size(arg);
+        let actual = GenomeSize::from_str(".77uB");
 
         assert!(actual.is_err());
     }
 
     #[test]
     fn empty_string_returns_error() {
-        let arg = String::from("");
-
-        let actual = parse_genome_size(arg);
+        let actual = GenomeSize::from_str("");
 
         assert!(actual.is_err());
     }
 
     #[test]
     fn suffix_with_no_size_returns_error() {
-        let arg = String::from("gb");
-
-        let actual = parse_genome_size(arg);
+        let actual = GenomeSize::from_str("gb");
 
         assert!(actual.is_err());
     }
 
     #[test]
-    fn empty_string_is_invalid_genome_size() {
-        let arg = String::from("");
-
-        assert!(is_genome_size_valid(arg).is_err())
-    }
-
-    #[test]
-    fn int_only_is_valid() {
-        let arg = String::from("789");
-
-        assert!(is_genome_size_valid(arg).is_ok())
-    }
-
-    #[test]
-    fn float_only_is_valid() {
-        let arg = String::from("79.6");
-
-        assert!(is_genome_size_valid(arg).is_ok())
-    }
-
-    #[test]
-    fn int_with_valid_suffix_is_valid() {
-        let arg = String::from("79g");
-
-        assert!(is_genome_size_valid(arg).is_ok())
-    }
-
-    #[test]
-    fn int_with_invalid_suffix_is_invalid() {
-        let arg = String::from("79p");
-
-        assert!(is_genome_size_valid(arg).is_err())
-    }
-
-    #[test]
-    fn float_with_valid_suffix_is_valid() {
-        let arg = String::from("79.765t");
-
-        assert!(is_genome_size_valid(arg).is_ok())
-    }
-
-    #[test]
-    fn float_with_invalid_suffix_is_invalid() {
-        let arg = String::from("79.765eb");
-
-        assert!(is_genome_size_valid(arg).is_err())
-    }
-
-    #[test]
     fn int_coverage_returns_int() {
-        let arg = String::from("56");
-
-        let actual = parse_coverage(arg).unwrap();
+        let actual = Coverage::from_str("56").unwrap();
         let expected = 56 as u32;
 
         assert_eq!(actual, expected)
@@ -304,9 +254,7 @@ mod tests {
 
     #[test]
     fn float_coverage_returns_int() {
-        let arg = String::from("56.6");
-
-        let actual = parse_coverage(arg).unwrap();
+        let actual = Coverage::from_str("56.6").unwrap();
         let expected = 56 as u32;
 
         assert_eq!(actual, expected)
@@ -314,27 +262,21 @@ mod tests {
 
     #[test]
     fn empty_coverage_returns_err() {
-        let arg = String::from("");
-
-        let actual = parse_coverage(arg);
+        let actual = Coverage::from_str("");
 
         assert!(actual.is_err())
     }
 
     #[test]
     fn non_number_coverage_returns_err() {
-        let arg = String::from("foo");
-
-        let actual = parse_coverage(arg);
+        let actual = Coverage::from_str("foo");
 
         assert!(actual.is_err())
     }
 
     #[test]
     fn zero_coverage_returns_zero() {
-        let arg = String::from("0");
-
-        let actual = parse_coverage(arg).unwrap();
+        let actual = Coverage::from_str("0").unwrap();
         let expected = 0 as u32;
 
         assert_eq!(actual, expected)
@@ -342,9 +284,7 @@ mod tests {
 
     #[test]
     fn int_ending_in_x_coverage_returns_int() {
-        let arg = String::from("1X");
-
-        let actual = parse_coverage(arg).unwrap();
+        let actual = Coverage::from_str("1X").unwrap();
         let expected = 1 as u32;
 
         assert_eq!(actual, expected)
@@ -352,60 +292,9 @@ mod tests {
 
     #[test]
     fn float_ending_in_x_coverage_returns_int() {
-        let arg = String::from("1.9x");
-
-        let actual = parse_coverage(arg).unwrap();
+        let actual = Coverage::from_str("1.9X").unwrap();
         let expected = 1 as u32;
 
         assert_eq!(actual, expected)
-    }
-
-    #[test]
-    fn empty_string_is_invalid_coverage_string() {
-        let covg = String::from("");
-
-        assert!(is_coverage_valid(covg).is_err())
-    }
-
-    #[test]
-    fn zero_is_invalid_coverage_string() {
-        let covg = String::from("0");
-
-        assert!(is_coverage_valid(covg).is_err())
-    }
-
-    #[test]
-    fn int_is_valid_coverage_string() {
-        let covg = String::from("90");
-
-        assert!(is_coverage_valid(covg).is_ok())
-    }
-
-    #[test]
-    fn float_is_valid_coverage_string() {
-        let covg = String::from("90.9");
-
-        assert!(is_coverage_valid(covg).is_ok())
-    }
-
-    #[test]
-    fn word_is_invalid_coverage_string() {
-        let covg = String::from("foo");
-
-        assert!(is_coverage_valid(covg).is_err())
-    }
-
-    #[test]
-    fn int_ending_in_x_is_valid_coverage_string() {
-        let covg = String::from("50x");
-
-        assert!(is_coverage_valid(covg).is_ok())
-    }
-
-    #[test]
-    fn float_ending_in_x_is_valid_coverage_string() {
-        let covg = String::from("50.8X");
-
-        assert!(is_coverage_valid(covg).is_ok())
     }
 }
